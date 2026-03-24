c54ae1c55f09d22ae2e6670f59ab77cb037ed13f
Nicolas Mailloux <nicolecrivain@gmail.com>
2026-03-24 13:21:48 -0400

    support for multi-file tooltips

--- sage.sh
+++ sage.sh
@@ -4,9 +4,7 @@ export PATH="$PATH:/home/${USER}/miniforge3/envs/sage/bin"
 export PATH="$PATH:/Users/${USER}/miniforge3/envs/sage/bin/"
 
 clean() {
-	# echo "Complete directory listing before removal:"
-	# ls -shal "${TEMP}/${DIR}"
-	#rm -rf "${TEMP}"
+	rm -rf "${TEMP}"
 	kill -s TERM "${TOP_PID}"
 }
 
@@ -15,10 +13,10 @@ rm -rf ${TMPDIR}/tmp.*
 SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 LATEX_TEMP="/tmp/latex-temp"
 TEMP="$(mktemp -d)"
-DIRR="$(dirname ""${1}"")"
-DIR="$(basename ""${DIRR}"")"
-FILE="$(basename ""${1}"")"
-FILENAME="$(basename -s .tex ""${1}"")"
+DIRR="$(dirname "${1}")"
+DIR="$(basename "${DIRR}")"
+FILE="$(basename "${1}")"
+FILENAME="$(basename -s .tex "${1}")"
 export TOP_PID=$$
 
 for pid in $(ps -ax | grep sage.sh | grep -v grep | awk '{ print $1 }'); do
@@ -35,6 +33,11 @@ pushd "${TEMP}/${DIR}"
 latexindent "${TEMP}/${DIR}/${FILE}" > "${TEMP}/${DIR}/${FILE}.indent"
 mv "${TEMP}/${DIR}/${FILE}.indent" "${TEMP}/${DIR}/${FILE}"
 
+# Consolidate all \input files into the main file
+if command -v latexpand >/dev/null 2>&1; then
+	latexpand --keep-comments "${FILE}" > "${FILE}.tmp" && mv "${FILE}.tmp" "${FILE}"
+fi
+
 if [ -e "${TEMP}/${DIR}/.${FILE}_ee" ]; then
 	TOOLTIP_ARG=0
 	EXPAND_ARG=1
@@ -50,11 +53,6 @@ else
 fi
 
 if [ ${TOOLTIP_ARG} != 0 ] || [ ${EXPAND_ARG} != 0 ]; then
-	FILES_TO_PROCESS="$(grep -oP '\\input\{\K[^}]+' "${TEMP}/${DIR}/${FILE}" | grep -v "sage.tex" | grep -v "_preamble.tex" | grep -v "sage.texinput")"
-	for f in ${FILES_TO_PROCESS}; do
-		python3 "${SCRIPT_DIR}/preprocessing.py" ${EXPAND_ARG} ${TOOLTIP_ARG} "${TEMP}/${DIR}/${f}" "${TEMP}/${DIR}/${f}.f"
-		mv "${TEMP}/${DIR}/${f}.f" "${TEMP}/${DIR}/${f}"
-	done
 	python3 "${SCRIPT_DIR}/preprocessing.py" ${EXPAND_ARG} ${TOOLTIP_ARG} "${TEMP}/${DIR}/${FILE}" "${TEMP}/${DIR}/${FILE}.f"
 	mv "${TEMP}/${DIR}/${FILE}.f" "${TEMP}/${DIR}/${FILE}"
 fi
